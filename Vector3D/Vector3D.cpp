89ba9c411d3ebe6d0edfff6cd94228226944f8f8
Andriy Kobetiak <andriykobetiak@gmail.com>
2022-10-30 15:42:16 +0200

    rewrite to aggregation

--- Vector3D/Vector3D.cpp
+++ Vector3D/Vector3D.cpp
@@ -2,7 +2,7 @@
 // Created by Andriy Kobetiak on 25.10.2022.
 //
 
-#include <cmath>
+#include <iostream>
 #include "Vector3D.h"
 
 Vector3D::Vector3D(const Triad &triad) {
@@ -11,7 +11,7 @@ Vector3D::Vector3D(const Triad &triad) {
   this->triad.setZ(triad.getZ());
 }
 
-const Triad &Vector3D::getTriad() const {
+const Vector3D::Triad &Vector3D::getTriad() const {
   return triad;
 }
 
@@ -67,4 +67,122 @@ void Vector3D::read() {
 
 void Vector3D::display() const {
   triad.display();
+}
+
+int Vector3D::Triad::getX() const {
+  return x;
+}
+
+void Vector3D::Triad::setX(int x) {
+  Triad::x = x;
+}
+
+int Vector3D::Triad::getY() const {
+  return y;
+}
+
+void Vector3D::Triad::setY(int value) {
+  Vector3D::Triad::y = value;
+}
+
+int Vector3D::Triad::getZ() const {
+  return z;
+}
+
+void Vector3D::Triad::setZ(int value) {
+  Triad::z = value;
+}
+
+Vector3D::Triad::Triad(int x, int y, int z)
+    : x(x),
+      y(y),
+      z(z) {}
+
+bool Vector3D::Triad::operator==(const Vector3D::Triad &rhs) const {
+  return x == rhs.x &&
+         y == rhs.y &&
+         z == rhs.z;
+}
+
+bool Vector3D::Triad::operator!=(const Vector3D::Triad &rhs) const {
+  return !(rhs == *this);
+}
+
+void Vector3D::Triad::operator+=(const Vector3D::Triad &other) {
+  this->x += other.x;
+  this->y += other.y;
+  this->z += other.z;
+}
+
+Vector3D::Triad Vector3D::Triad::operator+(const Vector3D::Triad &other) {
+  Triad copy;
+  copy += other;
+
+  return copy;
+}
+
+void Vector3D::Triad::operator+=(const int &other) {
+  this->x += other;
+  this->y += other;
+  this->z += other;
+}
+
+Vector3D::Triad Vector3D::Triad::operator+(const int &other) {
+  Triad copy;
+  copy += other;
+
+  return copy;
+}
+
+void Vector3D::Triad::operator*=(const int &other) {
+  this->x *= other;
+  this->y *= other;
+  this->z *= other;
+}
+
+Vector3D::Triad Vector3D::Triad::operator*(const int &other) {
+  Triad copy;
+  copy *= other;
+
+  return copy;
+}
+
+void Vector3D::Triad::operator*=(const Vector3D::Triad &other) {
+  this->x *= other.x;
+  this->y *= other.y;
+  this->z *= other.z;
+}
+
+Vector3D::Triad Vector3D::Triad::operator*(const Vector3D::Triad &other) {
+  Triad copy;
+  copy *= other;
+
+  return copy;
+}
+
+std::string Vector3D::Triad::toString() const {
+  return std::string("{ x: " + std::to_string(getX())
+                     + ", y: " + std::to_string(getY())
+                     + ", z: " + std::to_string(getZ()) + " }");
+}
+
+Vector3D::Triad& Vector3D::Triad::operator=(const Vector3D::Triad& other) {
+  if (this!=&other)
+  {
+    x = other.getX();
+    y = other.getY();
+    z = other.getZ();
+  }
+
+  return *this;
+}
+
+void Vector3D::Triad::read() {
+  std::cout << "X: "; std::cin >> x;
+  std::cout << "Y: "; std::cin >> y;
+  std::cout << "Z: "; std::cin >> z;
+}
+
+void Vector3D::Triad::display() const {
+  std::cout << toString() << std::endl;
 }
--- Vector3D/Vector3D.h
+++ Vector3D/Vector3D.h
@@ -2,21 +2,62 @@
 // Created by Andriy Kobetiak on 25.10.2022.
 //
 
-#ifndef OOP_LAB_5_VECTOR3D_H
-#define OOP_LAB_5_VECTOR3D_H
+#ifndef OOP_LAB_6_VECTOR3D_H
+#define OOP_LAB_6_VECTOR3D_H
 
-#include "../Triad/Triad.h"
+#include <string>
 
 class Vector3D {
   private:
-    Triad triad;
+    class Triad {
+      public:
+        explicit Triad(int x = 0, int y = 0, int z = 0);
+
+        int getX() const;
+        void setX(int x);
+
+        int getY() const;
+        void setY(int y);
+
+        int getZ() const;
+        void setZ(int value);
+
+        bool operator==(const Triad &rhs) const;
+        bool operator!=(const Triad &rhs) const;
+
+        void operator+=(const Triad& other);
+        Triad operator+(const Triad& other);
+
+        void operator+=(const int &other);
+        Triad operator+(const int &other);
+
+        void operator*=(const Triad &rhs);
+        Triad operator*(const Triad &rhs);
+
+        void operator*=(const int &other);
+        Triad operator*(const int &other);
+
+        void read();
+        void display() const;
+        std::string toString() const;
+        Triad& operator=(const Triad& other);
+
+      private:
+        int x = 0;
+        int y = 0;
+        int z = 0;
+    };
+
+  private:
+    Vector3D::Triad triad;
+
   public:
-    explicit Vector3D(const Triad &triad);
+    explicit Vector3D(const Vector3D::Triad &triad);
     explicit Vector3D(int x, int y = 0, int z = 0);
     Vector3D();
 
-    const Triad &getTriad() const;
-    void setTriad(const Triad &triad);
+    const Vector3D::Triad &getTriad() const;
+    void setTriad(const Vector3D::Triad &triad);
 
     void operator+=(const Vector3D& other);
     Vector3D operator+(const Vector3D& other);
@@ -29,4 +70,4 @@ class Vector3D {
 };
 
 
-#endif //OOP_LAB_5_VECTOR3D_H
+#endif //OOP_LAB_6_VECTOR3D_H
